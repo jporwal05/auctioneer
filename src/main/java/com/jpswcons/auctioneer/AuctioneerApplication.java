a0aff91c70b8ba36f50d79843267d347d3279486
Jayant Porwal <jayantporwal05@gmail.com>
2023-08-03 11:38:46 +0530

    skeleton

---
+++ src/main/java/com/jpswcons/auctioneer/AuctioneerApplication.java
@@ -0,0 +1,13 @@
+package com.jpswcons.auctioneer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctioneerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctioneerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpswcons/auctioneer/AuctioneerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpswcons.auctioneer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctioneerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
